3cb65b419427877eb9d0ce548a47d5409912b6f8
ma <442026460@qq.com>
2021-06-16 08:07:26 +0800

    保存

---
+++ src/components/Button/button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export enum ButtonSize {
+  Large = 'lg',
+  Small = 'sm'
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link'
+}
+
+interface BaseButtonProps {
+  
+}
